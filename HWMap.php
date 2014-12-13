ecf670cb2d6c764b49d1e9fe83bd9f30a021472d
Remigr <remiclaude@free.fr>
2014-12-13 19:43:25 +0200

    Change name to HWMap

---
+++ HWMap.php
@@ -0,0 +1,43 @@
+<?php
+if ( !defined( 'MEDIAWIKI' ) ) die();
+/**
+ * hitchwikimap extension
+ * <hitchmap></hitchmap>
+ */
+
+$wgExtensionCredits['hitchwikimap'][] = array(
+	'path' => __FILE__,
+	'name' => 'HWMap',
+	'author' => 'Rémi Claude'
+);
+
+$wgAutoloadClasses['HWMap'] = $IP . '/extensions/HWMap/HWMap.php';
+$wgHooks['ParserFirstCallInit'][] = 'onParserInit';
+
+// Register modules
+$wgResourceModules['ext.HWMap'] = array(
+	'scripts' => array(
+        'modules/vendor/leaflet/dist/leaflet.js',
+        'modules/ext.HWMap.js'
+	),
+	'styles' => array(
+        'modules/vendor/leaflet/dist/leaflet.css',
+		'modules/ext.HWMap.css'
+	),
+	'localBasePath' =>  __DIR__,
+	'remoteExtPath' => 'HWMap'
+);
+
+/**
+ * The registration function.
+ */
+function onParserInit( Parser $parser ) {
+    global $wgOut;
+    $parser->setHook( 'hwmap','HWMapRender'); 
+    $wgOut->addModules( 'ext.HWMap' );		
+    return true;
+}
+function HWMapRender( $input, array $args, Parser $parser, PPFrame $frame ) {
+    return '<div id="hwmap"></div>';
+}
+
--- modules/ext.HWMap.js
+++ modules/ext.HWMap.js
@@ -1,19 +1,19 @@
 //Root locations
 var api_root = mw.config.get("wgServer") + mw.config.get("wgScriptPath");
-var extension_root = mw.config.get("wgExtensionAssetsPath") + "/HitchwikiMap/";
+var extension_root = mw.config.get("wgExtensionAssetsPath") + "/HWMap/";
 
 //Title of the current page
 var pageTitle = mw.config.get("wgTitle");
 
 //Setting up the map
-var hitchmap = L.map('hitchmap');
+var hwmap = L.map('hwmap');
 
 L.Icon.Default.imagePath = extension_root + 'modules/vendor/leaflet/dist/images';
 
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/remigr.jol982fa/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
-}).addTo(hitchmap);
+}).addTo(hwmap);
 
 //Getting the current coordinate
 $.get( api_root + "/api.php?action=query&prop=coordinates&titles=" + pageTitle + "&format=json", function( data ) {
@@ -21,18 +21,18 @@ $.get( api_root + "/api.php?action=query&prop=coordinates&titles=" + pageTitle +
         page = data.query.pages[i];
         break;
     }
-    L.marker([page.coordinates[0].lat, page.coordinates[0].lon]).addTo(hitchmap);
-    hitchmap.setView([page.coordinates[0].lat, page.coordinates[0].lon], 10);
+    L.marker([page.coordinates[0].lat, page.coordinates[0].lon]).addTo(hwmap);
+    hwmap.setView([page.coordinates[0].lat, page.coordinates[0].lon], 10);
 });
 
 //Getting related spots
 $.get( api_root + "/api.php?action=ask&query=[[Category:Spots]][[Cities::" + pageTitle + "]]|%3FLocation&format=json", function( data ) {
     console.log(data);
     for (var i in data.query.results) {
-        L.marker([data.query.results[i].printouts.Location[0].lat,data.query.results[i].printouts.Location[0].lon]).addTo(hitchmap);
+        L.marker([data.query.results[i].printouts.Location[0].lat,data.query.results[i].printouts.Location[0].lon]).addTo(hwmap);
     }
 });
 
-hitchmap.on('moveend', function() {
-    console.log(hitchmap.getBounds());
+hwmap.on('moveend', function() {
+    console.log(hwmap.getBounds());
 });
