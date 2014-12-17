dac803d9c78b3f9b464ccfd59b3965ca156e17fe
Remigr <remiclaude@free.fr>
2014-12-17 12:20:37 +0200

    Add title to special page

--- SpecialHWMap.php
+++ SpecialHWMap.php
@@ -6,6 +6,7 @@ class SpecialHWMap extends SpecialPage {
 
 	function execute( $par ) {
         $output = $this->getOutput();
+        $output->setPageTitle('Hitchwiki Map');
         $output->addModules( 'ext.HWMap' );
         $output->addWikiText('<div id="hwmap"></div>');
 	}
