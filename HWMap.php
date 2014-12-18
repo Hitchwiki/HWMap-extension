fd9903a5888d3d0a344d4356f6be5afcd0d42a51
Remigr <remiclaude@free.fr>
2014-12-18 18:23:01 +0200

    Merge

--- HWMap.php
+++ HWMap.php
@@ -5,14 +5,25 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
  * <hwmap></hwmap>
  */
 
+// Kudos
 $wgExtensionCredits['hitchwikimap'][] = array(
 	'path' => __FILE__,
 	'name' => 'HWMap',
-	'author' => 'Rémi Claude'
+	'descriptionmsg' => 'hwmaps-desc',
+	'author' => array('Rémi Claude', 'Mikael Korpela'),
+	'url' => 'https://github.com/Hitchwiki/HWMap-extension',
+	'version' => '2.0.0',
+	'license-name' => 'MIT'
 );
 
-//Register hook
+// Register hook
 $wgHooks['ParserFirstCallInit'][] = 'onParserInit';
+
+// Extension & magic words i18n
+$wgMessagesDirs['HWMap'] = __DIR__ . '/i18n';
+
+// Register aliases
+$wgExtensionMessagesFiles['HWMapAlias'] = __DIR__ . '/HWMap.alias.php';
 
 // Register special pages
 $wgAutoloadClasses['SpecialHWMap'] = $IP . '/extensions/HWMap/SpecialHWMap.php';
@@ -21,6 +32,7 @@ $wgSpecialPages['HWMap'] = 'SpecialHWMap';
 // Register modules
 $wgResourceModules['ext.HWMap'] = array(
 	'scripts' => array(
+<<<<<<< HEAD
         'modules/vendor/leaflet/dist/leaflet.js',
         'modules/vendor/leaflet.markercluster/dist/leaflet.markercluster.js',
         'modules/ext.HWMap.js'
@@ -29,6 +41,13 @@ $wgResourceModules['ext.HWMap'] = array(
         'modules/vendor/leaflet/dist/leaflet.css',
         'modules/vendor/leaflet.markercluster/dist/MarkerCluster.css',
         'modules/vendor/leaflet.markercluster/dist/MarkerCluster.Default.css',
+=======
+    'modules/vendor/leaflet/dist/leaflet.js',
+    'modules/ext.HWMap.js'
+	),
+	'styles' => array(
+    'modules/vendor/leaflet/dist/leaflet.css',
+>>>>>>> df08210dca6071d80579806c324eeb7397ac67aa
 		'modules/ext.HWMap.css'
 	),
 	'localBasePath' =>  __DIR__,
@@ -39,12 +58,11 @@ $wgResourceModules['ext.HWMap'] = array(
  * The hook registration function.
  */
 function onParserInit( Parser $parser ) {
-    global $wgOut;
-    $parser->setHook( 'hwmap','HWMapRender'); 
-    $wgOut->addModules( 'ext.HWMap' );		
-    return true;
+  global $wgOut;
+  $parser->setHook( 'hwmap', 'HWMapRender');
+  $wgOut->addModules( 'ext.HWMap' );
+  return true;
 }
 function HWMapRender( $input, array $args, Parser $parser, PPFrame $frame ) {
-    return '<div id="hwmap"></div>';
+  return '<div id="hwmap"></div>';
 }
-
--- SpecialHWMap.php
+++ SpecialHWMap.php
@@ -1,13 +1,13 @@
 <?php
 class SpecialHWMap extends SpecialPage {
-	function __construct() {
-		parent::__construct( 'HWMap' );
-	}
+  function __construct() {
+    parent::__construct( 'HWMap' );
+  }
 
-	function execute( $par ) {
-        $output = $this->getOutput();
-        $output->setPageTitle('Hitchwiki Map');
-        $output->addModules( 'ext.HWMap' );
-        $output->addWikiText('<div id="hwmap"></div>');
-	}
+  function execute( $par ) {
+    $output = $this->getOutput();
+    $output->setPageTitle( 'Hitchwiki Map' );
+    $output->addModules( 'ext.HWMap' );
+    $output->addWikiText('<div id="hwmap"></div>');
+  }
 }
