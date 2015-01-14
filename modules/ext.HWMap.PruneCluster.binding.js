b5b2a3dfabdc7c3da7df03cb1560adf5aed311ea
Mikael Korpela <mikael@ihminen.org>
2015-01-15 00:27:51 +0200

    Make sure PruneCluster is global (window)

---
+++ modules/ext.HWMap.PruneCluster.binding.js
@@ -0,0 +1,6 @@
+/*
+ * Make sure PruneCluster is global (window)
+ * @link https://www.mediawiki.org/wiki/ResourceLoader/Migration_guide_for_extension_developers#Troubleshooting_and_tips
+ */
+window.PruneClusterForLeaflet = PruneClusterForLeaflet;
+window.PruneClusterLeafletSpiderfier = PruneClusterLeafletSpiderfier;
